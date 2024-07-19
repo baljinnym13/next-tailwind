3ee7e424cf026589e8a67584f107f5473f31a1f2
baljinnym13 <baljinnym1318@gmail.com>
2024-07-19 12:14:05 +0800

    sdf

---
+++ components/header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1 className="font-bold">Brainwave.io</h1>
+        <ul>
+          <li>Demos</li>
+          <li>Pages</li>
+          <li>Support</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <button>Get started a project</button>
+    </header>
+  );
+};
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    /** @type {import('tailwindcss').Config} */
+    (module.exports = {
+      content: ["./src/**/*.{html,js}"],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
